c09d38437ad890472677155b7e0808c572ecf26e
Anuj Datar <anuj.datar@gmail.com>
2021-10-08 19:40:22 +0530

    Code refactor and cleanup. Made variable names more descriptive/accurate

--- batch.py
+++ batch.py
@@ -1,11 +1,6 @@
 import tkinter as tk
 
 from batch_gui import MainAppGui
-# from participant_class import Participant
-# from write_to_target_sheet import write_to_target_sheet
-
-datasheet_path = './SampleDataSheet.xlsx'
-target_file_path = './Hebert_Data Variables.xlsx'
 
 if __name__ == '__main__':
     root = tk.Tk()
--- batch_gui.py
+++ batch_gui.py
@@ -9,38 +9,38 @@ class MainAppGui(object):
     def __init__(self, master=None):
         self.master = master
 
-        self.folder_name = tk.StringVar()
-        self.filename = tk.StringVar()
+        self.source_folder_name = tk.StringVar()
+        self.target_filename = tk.StringVar()
 
         # self.master.minsize(width=640, height=480)
         self.master.resizable(True, True)
         self.master.title("Python Data Extractor")
 
-        self.folder_selector = ttk.Label(self.master, text="Select Data")
-        self.folder_selector.grid(row=0, column=0, padx=5, pady=5)
+        self.source_folder_selector_label = ttk.Label(self.master, text="Select Source Folder")
+        self.source_folder_selector_label.grid(row=0, column=0, padx=5, pady=5)
 
-        self.folder_selector_entry = ttk.Entry(self.master, textvariable=self.folder_name)
-        self.folder_selector_entry.grid(row=0, column=1, ipadx=80, padx=5, pady=5)
+        self.source_folder_selector_entry = ttk.Entry(self.master, textvariable=self.source_folder_name)
+        self.source_folder_selector_entry.grid(row=0, column=1, ipadx=80, padx=5, pady=5)
 
-        self.folder_selector_button = ttk.Button(
+        self.source_folder_selector_button = ttk.Button(
             self.master,
-            text="Select Folder",
+            text="Browse",
             command=self.select_source_folder
         )
-        self.folder_selector_button.grid(row=0, column=3, padx=5, pady=5)
+        self.source_folder_selector_button.grid(row=0, column=3, padx=5, pady=5)
 
-        self.file_selector = ttk.Label(self.master, text="Select Target File")
-        self.file_selector.grid(row=1, column=0, padx=5, pady=5)
+        self.target_file_selector_label = ttk.Label(self.master, text="Select Target File")
+        self.target_file_selector_label.grid(row=1, column=0, padx=5, pady=5)
 
-        self.file_selector_entry = ttk.Entry(self.master, textvariable=self.filename)
-        self.file_selector_entry.grid(row=1, column=1, ipadx=80, padx=5, pady=5)
+        self.target_file_selector_entry = ttk.Entry(self.master, textvariable=self.target_filename)
+        self.target_file_selector_entry.grid(row=1, column=1, ipadx=80, padx=5, pady=5)
 
-        self.file_selector_button = ttk.Button(
+        self.target_file_selector_button = ttk.Button(
             self.master,
-            text="Select File",
+            text="Browse",
             command=self.select_target_file
         )
-        self.file_selector_button.grid(row=1, column=3, padx=5, pady=5)
+        self.target_file_selector_button.grid(row=1, column=3, padx=5, pady=5)
 
         self.execute_button = ttk.Button(
             self.master,
@@ -53,11 +53,11 @@ class MainAppGui(object):
         """
         Open a folder selection dialog box
         """
-        folder_name = filedialog.askdirectory(
+        source_folder_name = filedialog.askdirectory(
             title='Select Folder',
             initialdir='./'
         )
-        self.folder_name.set(folder_name)
+        self.source_folder_name.set(source_folder_name)
 
     def select_target_file(self):
         """
@@ -66,16 +66,16 @@ class MainAppGui(object):
         filetypes = [
             ('Excel', '.xlsx')
         ]
-        filename = filedialog.askopenfilename(
+        target_filename = filedialog.askopenfilename(
             title='Select Target File',
             initialdir='./',
             filetypes=filetypes
         )
-        self.filename.set(filename)
+        self.target_filename.set(target_filename)
 
     def run_program(self):
         # get file and folder name as strings
-        data_folder_name = self.folder_name.get()
-        target_filename = self.filename.get()
+        source_folder_name = self.source_folder_name.get()
+        target_filename = self.target_filename.get()
 
-        batch_extract(data_folder_name, target_filename)
+        batch_extract(source_folder_name, target_filename)
