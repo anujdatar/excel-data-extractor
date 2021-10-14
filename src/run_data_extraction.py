4562b9c35de1431184413d5cb84d3a11d7f66e7e
Anuj Datar <anuj.datar@gmail.com>
2021-10-14 11:16:28 +0530

    Updated single and batch extraction methods to use the new abstract 'extract' function

--- src/run_data_extraction.py
+++ src/run_data_extraction.py
@@ -1,15 +1,21 @@
 import os
 import pathlib
 
-from src import Participant
-from src import write_to_target_sheet
+from src import extract
 
 
-def batch_extract(source_folder_name: str, target_filename: str):
+def batch_extract(
+        vars_file: str,
+        source_folder_name: str,
+        target_filename: str,
+        target_sheet: str
+):
     """
     Handle extraction in batch mode, from source folder
+    :param vars_file: str -> absolute path to variable definition csv
     :param source_folder_name: str -> absolute path of source folder containing excel files
     :param target_filename: str -> absolute path of target excel file
+    :param target_sheet: str -> name of worksheet in target excel workbook
     """
     if not source_folder_name:
         print('please select source data folder')
@@ -21,14 +27,21 @@ def batch_extract(source_folder_name: str, target_filename: str):
         if pathlib.Path(file).suffix == '.xlsx':
             if '~$' not in pathlib.Path(file).name:
                 filepath = source_folder_name + '/' + file
-                extract(filepath, target_filename)
+                extract(vars_file, filepath, target_filename, target_sheet)
 
 
-def single_extract(source_filename: str, target_filename: str):
+def single_extract(
+        vars_file: str,
+        source_filename: str,
+        target_filename: str,
+        target_sheet: str
+):
     """
     Handle extraction in single file mode.
+    :param vars_file: str -> absolute path to variable definition csv
     :param source_filename: str -> absolute path of source excel file
     :param target_filename: str -> absolute path of target excel file
+    :param target_sheet: str -> name of worksheet in target excel workbook
     """
     if not source_filename:
         print('please select source data file')
@@ -38,15 +51,4 @@ def single_extract(source_filename: str, target_filename: str):
 
     if pathlib.Path(source_filename).suffix == '.xlsx':
         if '~$' not in pathlib.Path(source_filename).name:
-            extract(source_filename, target_filename)
-
-
-def extract(source_file: str, target_file: str):
-    """
-    Extract data from source excel file and write to target file
-    :param source_file: str -> absolute path of source excel file
-    :param target_file: str -> absolute path of target excel file
-    """
-    # fetch data
-    participant_data = Participant(source_file)
-    write_to_target_sheet(target_file, participant_data)
+            extract(vars_file, source_filename, target_filename, target_sheet)
